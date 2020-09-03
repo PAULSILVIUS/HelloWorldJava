ed7bfbfb31f9ed103d9a16163c0e99269192fda4
Paul Silvius <paulsilvius0@gmail.com>
2020-09-03 13:11:27 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+
+//Hello World geared to CISC 3130
+//edited to make program unique for my fall 2020 class
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, CISC-3130!"); 
+    }
+}
